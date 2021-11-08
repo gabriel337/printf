0f73e11e8692c4033f49fd227c131a298464bede
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-08 23:54:02 +0000

    added printf file

---
+++ _printf.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int _printf(const char *format, ...)
+{
+	unsigned int i, result;
+	va_list vl;
+
+	va_start(vl, format); /* pointing to format */
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%') /*if i is the same as % then verify the next char and call the function*/
+		{
+			result += get_print(format[i + 1], &ap);
+			i++;
+		}
+		else /*if not then print each char (string)*/
+		{
+			_putchar(format[i]);
+			result++;
+		}
+	}
+	va_end(vl);
+	return (result);
+}
