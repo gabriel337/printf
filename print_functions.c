78a26779bd86f423fb386816e6665f4d4212a7e9
Joanfa95 <3235@holbertonschool.com>
2021-11-09 17:17:26 +0000

    fixed betty

--- print_functions.c
+++ print_functions.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* print_string - prints string
-* @str: string to be printed
-* Return: returns the string
-*/
+ * print_string - prints string
+ * @str: string to be printed
+ * Return: returns the string
+ */
 int print_string(char *str)
 {
 	unsigned int i;
@@ -29,13 +29,13 @@ int _putchar(char c)
 int print_int(int num)
 {
 	if (num < 0)
-    {
-       _putchar('-');
-       num = -num;
-    }
-	 if (num > 9)
 	{
-	   print_int(num / 10);
+		_putchar('-');
+		num = -num;
+	}
+	if (num > 9)
+	{
+		print_int(num / 10);
 
 	}
 	_putchar(num % 10 + '0');
