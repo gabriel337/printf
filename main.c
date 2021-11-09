cfcea09e905d539e0b99eda68da6ad7dbec3c0c6
gabriel337 <g.david.lopezruiz@gmail.com>
2021-11-09 17:46:35 +0000

    fixed bugs

--- main.c
+++ main.c
@@ -20,8 +20,10 @@ int main(void)
 	addr = (void *)0x7ffe637541f0;
 	_printf("Length:[%d, %i]\n", len, len);
 	printf("Length:[%d, %i]\n", len2, len2);
-	_printf("Negative:[%d]\n", -762534);
-	printf("Negative:[%d]\n", -762534);
+	len = _printf("Negative:[%d]\n", -762534);
+	_printf("Len:[%d]\n", len);
+	len2 = printf("Negative:[%d]\n", -762534);
+	printf("Len:[%d]\n", len2);
 	_printf("Unsigned:[%u]\n", ui);
 	printf("Unsigned:[%u]\n", ui);
 	_printf("Unsigned octal:[%o]\n", ui);
@@ -36,8 +38,6 @@ int main(void)
 	printf("Address:[%p]\n", addr);
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
-	_printf("Len:[%d]\n", len);
-	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
 	printf("Unknown:[%r]\n");
 	return (0);
--- print_functions.c
+++ print_functions.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* print_string - prints string
-* @str: string to be printed
-* Return: returns the string
-*/
+ * print_string - prints string
+ * @str: string to be printed
+ * Return: returns the string
+ */
 int print_string(char *str)
 {
 	unsigned int i;
@@ -26,18 +26,25 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
+/**
+ *print_init - prints numbers
+ @num: number to be printed
+ *Return: num
+ */
 int print_int(int num)
 {
-	if (num < 0)
-    {
-       _putchar('-');
-       num = -num;
-    }
-	 if (num > 9)
-	{
-	   print_int(num / 10);
+	int i = 1;
 
+	if (num < 0)
+	{
+		_putchar('-');
+		i++;
+		num = -num;
+	}
+	if (num > 9)
+	{
+		i += print_int(num / 10);
 	}
 	_putchar(num % 10 + '0');
-	return (num);
+	return (i);
 }
