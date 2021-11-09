8775dacd85fb31ea4c8e8ecfcd982d962566df19
Joanfa95 <3235@holbertonschool.com>
2021-11-09 00:20:09 +0000

    fix bug

--- main.h
+++ main.h
@@ -3,17 +3,17 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdid.h>
+#include <stdlid.h>
 #include <unistd.h>
 
-int _print(const char *format, ...);/*the function identifies the the fist charater of the string
+int _printf(const char *format, ...);/*the function identifies the the fist charater of the string
 										send it to the correct funciotn to be porcesed*/
-int pull_print(char c, valist *vl);/**/
+int pull_print(char c, va_list *vl);/**/
 
-int p_string(char*); /* Function to print string*/
+int print_string(char*); /* Function to print string*/
 int p_int(int); /*Function that prints integers*/
 int _putchar(char);/*Function that prints characters*/
-int exponent(int x int y);/**/
+int exponent(int x, int y);/**/
 
 int mkint(va_list *vl);/**/
 int mkstring(va_list *vl);/**/
@@ -30,5 +30,5 @@ typedf struct flag
 	char letter;
 	int (*prnt)(va_list);
 }find_flag;
-#endif/*_MAINH*/
+#endif/_MAINH*/
 
